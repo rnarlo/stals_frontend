c83fd9253961802b8d60f954c74702c23e20c5f9
cherrycontaoi <cbcontaoi@up.edu.ph>
2023-05-12 06:00:16 +0800

    Changed the sign in page Ui

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -8,13 +8,13 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  final GlobalKey<FormState> emailKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> passKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    final GlobalKey<FormState> emailKey = GlobalKey<FormState>();
-    final GlobalKey<FormState> passKey = GlobalKey<FormState>();
-    TextEditingController emailController = TextEditingController();
-    TextEditingController passwordController = TextEditingController();
-
     final email = Form(
         key: emailKey,
         child: TextFormField(
@@ -26,8 +26,13 @@ class _SignInPageState extends State<SignInPage> {
             return null;
           },
           decoration: const InputDecoration(
-            border: OutlineInputBorder(),
-            contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(18)),
+                borderSide:
+                    const BorderSide(width: 0, style: BorderStyle.none)),
+            filled: true,
+            fillColor: Colors.white,
+            contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 18),
             labelText: 'E-mail',
           ),
         ));
@@ -44,8 +49,13 @@ class _SignInPageState extends State<SignInPage> {
           },
           obscureText: true,
           decoration: const InputDecoration(
-            border: OutlineInputBorder(),
-            contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(18)),
+                borderSide:
+                    const BorderSide(width: 0, style: BorderStyle.none)),
+            filled: true,
+            fillColor: Colors.white,
+            contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 18),
             labelText: 'Password',
           ),
         ));
@@ -79,7 +89,7 @@ class _SignInPageState extends State<SignInPage> {
         );
       },
       child: const Text(
-        'Make an Account!',
+        'Make an account!',
         style: TextStyle(color: Color.fromARGB(255, 25, 83, 95)),
       ),
     ));
@@ -104,7 +114,7 @@ class _SignInPageState extends State<SignInPage> {
       child: Column(
         children: [
           email,
-          const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 8)),
           password,
           const Padding(padding: EdgeInsets.symmetric(vertical: 15)),
           loginButton,
@@ -112,43 +122,58 @@ class _SignInPageState extends State<SignInPage> {
           const Text(
             "No account yet?",
             textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: 12, color: Color.fromARGB(255, 240, 243, 245)),
+            style:
+                TextStyle(fontSize: 12, color: Color.fromARGB(255, 31, 36, 33)),
           ),
-          const Padding(padding: EdgeInsets.symmetric(vertical: 3)),
+          const Padding(padding: EdgeInsets.symmetric(vertical: 2)),
           signupButton
         ],
       ),
     );
 
     return Scaffold(
-        body: Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-        colors: [
-          Color.fromARGB(255, 240, 243, 245),
-          Color.fromARGB(255, 25, 83, 95)
-        ],
-        stops: [0.35, 0.95],
-      )),
-      child: Center(
-        child: ListView(
+        body: SingleChildScrollView(
+            child: Column(
+      children: [
+        const Padding(padding: EdgeInsets.symmetric(vertical: 70)),
+        SizedBox(
+            child:
+                Image.asset('assets/images/stals_logo2.png', fit: BoxFit.fill)),
+        const Padding(
+          padding: EdgeInsets.only(left: 45, top: 20),
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              "Welcome Back",
+              style: TextStyle(
+                  fontSize: 28,
+                  // fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 31, 36, 33)),
+            ),
+          ),
+        ),
+        ListView(
           shrinkWrap: true,
           padding: const EdgeInsets.only(left: 40.0, right: 40.0),
           children: <Widget>[
+            // Image.asset('assets/images/stals_logo.png', fit: BoxFit.fill),
             const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-            const Text(
-              "Welcome Back!",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-            ),
-            const Padding(padding: EdgeInsets.symmetric(vertical: 30)),
             loginFields
           ],
         ),
-      ),
-    ));
+      ],
+    ))
+        // decoration: const BoxDecoration(
+        //     gradient: LinearGradient(
+        //   begin: Alignment.topCenter,
+        //   end: Alignment.bottomCenter,
+        //   colors: [
+        //     Color.fromARGB(255, 240, 243, 245),
+        //     Color.fromARGB(255, 25, 83, 95)
+        //   ],
+        //   stops: [0.35, 0.95],
+        // )),
+
+        );
   }
 }
